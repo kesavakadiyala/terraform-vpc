dc4c2555865acc87ee3abd0daeb6d473c7b148c5
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-02-27 21:47:59 +0530

    Added Outputs

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "PRIVATE_SUBNET" {
+  value = aws_subnet.private_subnet.*.id
+}
+
+output "PUBLIC_SUBNET" {
+  value = aws_subnet.public_subnet.*.id
+}
+
+output "PRIVATE_CIDR" {
+  value = aws_subnet.private_subnet.*.cidr_block
+}
+
+output "PUBLIC_CIDR" {
+  value = aws_subnet.private_subnet.*.cidr_block
+}

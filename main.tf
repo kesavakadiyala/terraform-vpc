5fdb5fa612baf26c1604a67fde7b1859f5acd8cb
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-09 15:40:22 +0530

    Bug Fixes

--- main.tf
+++ main.tf
@@ -30,8 +30,7 @@ resource "aws_subnet" "private_subnet" {
 }
 
 resource "aws_route53_record" "backend-route53-dev" {
-  count1              = var.ENV == "dev" ? 1 : 0
-  count               = length(var.BACKEND_COMPONENTS)
+  count               = length(var.BACKEND_COMPONENTS) && var.ENV == "dev" ? 1 : 0
   name                = "${element(var.BACKEND_COMPONENTS, count.index)}-${var.ENV}"
   type                = "CNAME"
   zone_id             = var.HOSTED_ZONE_ID
@@ -40,8 +39,7 @@ resource "aws_route53_record" "backend-route53-dev" {
 }
 
 resource "aws_route53_record" "backend-route53-prod" {
-  count1              = var.ENV == "prod" ? 1 : 0
-  count               = length(var.BACKEND_COMPONENTS)
+  count               = length(var.BACKEND_COMPONENTS) && var.ENV == "prod" ? 1 : 0
   name                = "${element(var.BACKEND_COMPONENTS, count.index)}-${var.ENV}"
   type                = "CNAME"
   zone_id             = var.HOSTED_ZONE_ID

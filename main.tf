4059631ef492e103a1916275265a48f9254fb9d3
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-09 16:58:26 +0530

    Removing ENV variable

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block         = var.VPC_CIDR
   tags               = {
-    Name             = "Roboshop_VPC-${var.ENV}"
-    Environment      = var.ENV
+    Name             = "Roboshop_VPC"
   }
 }
 
